0223e5dd4aa65158a5ab072b4ccdedb6e5a811a4
tylermorrisford <tylerford@gmail,com>
2019-09-16 13:15:58 -0600

    add backToTop button//update readme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,6 +25,21 @@
           setInterval(function() {
             $('.carousel').carousel('next');
           }, 4000);
+
+          var btn = $('#backToTop');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+  });
         
   
     });
